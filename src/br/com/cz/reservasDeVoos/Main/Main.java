94c0dfb4d75366003c673e8745fc85f23960d69d
clara290 <mariaclarafigueiredo485@gmail.com>
2023-09-26 15:13:24 -0300

    teste

--- src/br/com/cz/reservasDeVoos/Main/Main.java
+++ src/br/com/cz/reservasDeVoos/Main/Main.java
@@ -6,9 +6,9 @@ import br.com.cz.reservasDeVoos.Model.Voo;
 
 public class Main {
     public static void main(String[] args) {
-        Aeroporto aeroporto = new Aeroporto("Aeroporto internacional de Congonhas", "Cidade de São Paulo");
+        Aeroporto aeroporto = new Aeroporto("Aeroporto nacional de Congonhas", "Cidade de São Paulo");
 
-        Voo voo1 = new Voo("V123", "São Paulo", "Londres", "2023-12-20  06:00", "2023-09-21  23:00", 200, 0);
+        Voo voo1 = new Voo("V123", "São Paulo", "Londres", "2023-12-20  06:00", "2023-12-21  23:00", 200, 0);
         Voo voo2 = new Voo("V542", "São Paulo", "Rio de Janeiro", "2023-11-25  13:00", "2023-11-25  17:00", 170, 0);
 
         aeroporto.adicionarVoo(voo1);
